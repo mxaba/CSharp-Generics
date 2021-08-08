10ca5c856c3afa5b565ca1a520a2420f175a3e61
mxaba <samuel.mcebo@gmail.com>
2021-08-08 13:59:12 +0200

    stackApp

---
+++ src/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,14 @@
+namespace WiredBrainCoffee.StackApp {
+    internal class SimpleStack<T> {
+        private T[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStack() => _items = new T[10];
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(T item) => _items[++_currentIndex] = item;
+
+        public T Pop() => _items[_currentIndex--];
+    }
+}
